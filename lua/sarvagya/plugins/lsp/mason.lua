6bd62a85eedabc208efe5b354026254751e66803
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-03-15 12:05:34 +0530

    setup linting

--- lua/sarvagya/plugins/lsp/mason.lua
+++ lua/sarvagya/plugins/lsp/mason.lua
@@ -33,12 +33,14 @@ return {
         })
 
         mason_tool_installer.setup({
-          ensure_installed = {
-            "prettier", -- prettier formatter
-            "stylua", -- lua formatter
-            "isort", -- python formatter
-            "black", -- python formatter
-          },
+            ensure_installed = {
+                "prettier", -- prettier formatter
+                "stylua", -- lua formatter
+                "isort", -- python formatter
+                "black", -- python formatter
+                "pylint", -- python linter
+                "eslint_d", -- js linter
+            },
         })
     end,
 }
